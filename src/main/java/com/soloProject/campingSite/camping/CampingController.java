3a681d8f67496d66c0cf5a9fed5a95dfcc00bab2
yusinh <yysh1020@gmail.com>
2024-05-27 17:45:46 +0900

    임시

--- src/main/java/com/soloProject/campingSite/camping/CampingController.java
+++ src/main/java/com/soloProject/campingSite/camping/CampingController.java
@@ -3,8 +3,16 @@ package com.soloProject.campingSite.camping;
 import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 @RequiredArgsConstructor
 public class CampingController {
+
+    @GetMapping("/update")
+    public String update() {
+
+        return "camping_update";
+    }
+
 }
